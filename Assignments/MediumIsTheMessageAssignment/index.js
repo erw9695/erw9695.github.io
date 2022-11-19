1f2e7d7c3e586c56819ef4024c28f2510f014c74
erw9695 <erw9695@nyu.edu>
2022-11-19 11:36:12 -0500

    Updated hw.

--- Assignments/MediumIsTheMessageAssignment/index.js
+++ Assignments/MediumIsTheMessageAssignment/index.js
@@ -64,6 +64,12 @@ function infiniteImages() {
     playImages();
 }
 
+let mainFrame = document.getElementById("mainFrame");
+
+function vanishMain() {
+    mainFrame.style.animation = "vanish 5s";
+}
+
 function endOfAudio() {
     audio.pause()
     document.getElementById("visualsText").style.display = "Block";
@@ -71,8 +77,46 @@ function endOfAudio() {
     text = "The phonetic alphabet forced the magic world of the ear to yield to the neutral world of the eye. Man was given an eye for an ear."
     playVisualTextType();
     setTimeout(()=>{infiniteImages()},10000);
+    setTimeout(()=>{vanishMain()},15000);
+    mainFrame.addEventListener("animationend",vanishMainEnd);
 }
 
+let secondFrame = document.getElementById("secondFrame");
+
+function playElectricTextType(obj) {
+    console.log("AAAs");
+    if (i < text.length) {
+        document.getElementById("electric").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(playElectricTextType, speed);
+    }
+}
+
+function vanishMainEnd() {
+    mainFrame.style.display = "None";
+    secondFrame.style.display = "Block";
+    i = 0;
+    text = "Electric circuitry profoundly involves men with one another. Information pours upon us, instantaneously and continuously. As soon as information is acquired, it is very rapidly replaced by still newer information. Our electrically-configured world has forced us to move from the habit of data classification to the mode of pattern recognition."
+    playElectricTextType();
+    setTimeout(slideInOne, 15000);
+}
+
+function slideInOne() {
+    document.getElementById("tech1").style.animation = "appear 5s";
+    document.getElementById("tech1").style.opacity = 1;
+    document.getElementById("tech1").addEventListener("animationend",slideInTwo);
+}
+
+function slideInTwo() {
+    document.getElementById("tech2").style.animation = "appear 5s";
+    document.getElementById("tech2").style.opacity = 1;
+    document.getElementById("tech2").addEventListener("animationend",slideInThree);
+}
+
+function slideInThree() {
+    document.getElementById("tech3").style.animation = "appear 5s";
+    document.getElementById("tech3").style.opacity = 1;
+}
 
 
 
